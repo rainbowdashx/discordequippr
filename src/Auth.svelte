d0ac4fd2a78ce6a8d5e1346903087bd3fa936be6
rainbowdashx <born_to_kill_666@hotmail.com>
2021-06-09 22:59:21 +0200

    stuffz

--- src/Auth.svelte
+++ src/Auth.svelte
@@ -1,22 +1,7 @@
 
 <script>
     import { authKey } from './store.js';
-
-    function setLocalStorage(){
-        try {
-            localStorage.setItem("___equippr_token",$authKey);
-        } catch (error) {
-        }
-    }
-
-    if (auth){
-        try {
-            let auth =  localStorage.getItem("___equippr_token");
-            $authKey = auth;
-        } catch (error) {
-        }
-    }
-
+    
 </script>
 
-<input type="text" placeholder="AUTH"  bind:value={$authKey} 	on:change={setLocalStorage}/>
+<input type="text" placeholder="AUTH"  bind:value={$authKey} 	/>
